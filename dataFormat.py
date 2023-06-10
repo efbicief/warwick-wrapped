2d9cbb29ed6ba14e45220b63cb796c5f02ff22e1
LoudShadow <josephmevans2002@gmail.com>
2023-06-10 17:48:34 +0100

    data framework

--- dataFormat.py
+++ dataFormat.py
@@ -1,11 +1,23 @@
 
+class SVG():
+    file:str
 
+class DisplayItem:
+    size:int
+    def getSize(self):
+        return self.size
+class Category:
+    name:str
+    image:SVG
+    items:list[DisplayItem]
 
-class userInfo:
+class User:
     name:str
     degree:str
     year_of_study:str
-    def __init__(self,authInfo) -> None:
+    info:list[Category]
+    def __init__(self,uuid) -> None:
+        
         self.name="Joseph Evans"
         self.degree="Computer Science"
         self.year_of_study=[
@@ -19,9 +31,40 @@ class userInfo:
         ][2] + " Year" 
 
 
-class UserData:
-    info:userInfo
 
-    def __init__(self,authInfo) -> None:
-        self.user_info = userInfo(authInfo)
+# e.g.   "You have submitted" "35" "assignments"
+class ThreePart(DisplayItem):
+    size=1
+    def __init__(self,first,second,third) -> None:
+        super().__init__()
+        self.first = first
+        self.second = second
+        self.third = third 
+
+# Class for a 5 part data structure
+# e.g.   "Your Highest module was" "Databases" "with a total of" "86" "%"
+class FivePart(DisplayItem):
+    size=1.3
+
+    first:str
+    second:str
+    third:str
+    fourth:str
+    fith:str
+    def __init__(self,first,second,third,fourth,fith) -> None:
+        super().__init__()
+        self.first=first
+        self.second=second
+        self.third=third
+        self.fourth=fourth
+        self.fith = fith
+
+#Class for generc images
+class Image(DisplayItem):
+    size=2
+
+    url:str
+    def __init__(self,url) -> None:
+        super().__init__()
+        self.url = url
 
