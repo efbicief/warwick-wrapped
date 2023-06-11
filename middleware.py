b577cb9044c5e6c7612dfe6d5e147c50d64ae515
AlexanderFurmston <funkeypigeon.mail@gmail.com>
2023-06-11 14:19:59 +0100

    get_data now gets name, courseName, yearOfStudy

--- middleware.py
+++ middleware.py
@@ -1,6 +1,15 @@
 from dataFormat import User
+import sso
+
 def get_data(uuid)-> User:
-    pass
+    member = sso.get_user_info(uuid)
+    courseDetails = member.get("studentCourseDetails")[0]
+    return User(
+        member.get("firstName"),
+        courseDetails.get("currentRoute").get("name"),
+        courseDetails.get("levelCode", 0),
+        []
+    )
 
 def get_temp_data(uuid)-> User:
     pass
