802ff57d3007e1cad68471125c5fee2940dc5821
LoudShadow <josephmevans2002@gmail.com>
2023-06-13 05:32:15 +0100

    Statrted using pylint

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-# pylint: disable=missing-function-docstring
+"""
+This is the main file for the web application. 
+It contains the routes for the webpages and the functions that are
+called when the user visits a page.
+"""
 from flask import Flask, render_template ,redirect
 import sso
-import dataFormat
-import sys
 import middleware
+from dataFormat import User
 
 app = Flask(__name__)
 app.config['ENV'] = 'development'
@@ -13,26 +16,27 @@ app.config['TESTING'] = True
 
 @app.route("/")
 def home():
+    """Homepage the first page visited."""
     return render_template('homepage.html')
-
-@app.route("/start")
-def start():
-    return render_template('start.html')
 
 @app.route("/PrivacyPolicy")
 def privacy():
+    """Privacy Policy page."""
     return render_template('PrivacyPolicy.html')
 
 @app.route("/oauth/info")
-def oauthInfo():
+def oauth_info():
+    """Imformation about OAuth."""
     return render_template('OAuthInfo.html')
 
 @app.route("/oauth/begin")
 def get_begin_oauth():
+    """Redirects to the OAuth page."""
     return sso.get_begin_oauth()
 
 @app.route("/oauth/authorised")
 def get_authorised_oauth():
+    """Recieves the OAuth token and redirects to the results page."""
     uuid = sso.get_authorised_oauth()
     response = redirect("/results", code=302)
     response.set_cookie( "uuid",uuid )
@@ -41,6 +45,7 @@ def get_authorised_oauth():
 
 @app.route("/oauth/endpoint")
 def get_endpoint():
+    """A temporary page to test the OAuth endpoint."""
     uuid = sso.get_uuid_from_cookie()
     assignments = sso.get_assignments(uuid)
     member = sso.get_user_info(uuid)
@@ -54,41 +59,38 @@ def get_endpoint():
     for item in assignments['historicAssignments']:
         assignment_names.append(item['name'])
 
-    return render_template('assignments.html', assignments=assignment_names, member=member, submissions=user_submissions)
-
-@app.route("/oauth/userInfo")
-def get_warwick_info():
-    return sso.get_warwick_info()
+    return render_template('assignments.html',\
+                        assignments=assignment_names,\
+                        member=member,\
+                        submissions=user_submissions)
 
 @app.route("/oauth/tabula/events/")
 def get_upcoming_events():
+    """temporary page to test the upcoming events endpoint."""
     return sso.get_upcoming_events()
 
 @app.route("/results")
 def loading():
+    """Renders the laodin page that calls /api/results."""
     return render_template('loading.html')
 
 @app.route("/api/results")
-def renderResults():
+def render_results():
+    """The primary page of the web application."""
     try:
         uuid = sso.get_uuid_from_cookie()
-    except:
+    except TypeError:
         return redirect("/", code=302)
-    if uuid==None:
+    if uuid is None:
         return redirect("/", code=302) 
-    
-    userData=middleware.convert_to_page(middleware.get_data(uuid))
+    user_data:User=middleware.convert_to_page(middleware.get_data(uuid))
     # try:
     #     userData=middleware.convert_to_page(middleware.get_data(uuid))
     # except:
     #     return redirect("/", code=302) 
 
-    return render_template('Results.html',userData=userData)
+    return render_template('Results.html',userData=user_data)
 
-
-# @app.route("/oauth/tabula/assignments/")
-# def get_assignments(uuid):
-#     return sso.get_assignments(uuid)  
 
 if __name__ == "__main__":
     app.run(debug=True)
